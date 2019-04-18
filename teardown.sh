e3ac54813d04b2c8ef83efb2f12f9256d213010a
Cloud User <cloud-user@ocp-demo.stack.maskedadmins.com>
2019-04-18 13:14:43 -0400

    updating setup.sh

--- teardown.sh
+++ teardown.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 pushd ~/go/src/github.com/openshift/installer/ && \
-bin/openshift-install --dir=initial destroy cluster && \
+if [[ -x bin/openshift-install ]]
+then
+  bin/openshift-install --dir=initial destroy cluster
+fi
 popd && \
 rm -rf ~/go/src/github.com/openshift/installer
 
